ddc8b6980c8b5fda5cc8d9095c33b26a9f52efe3
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-20 18:21:07 -0800

    Server cleanup

--- scripts/load-seed-data.js
+++ scripts/load-seed-data.js
@@ -12,14 +12,6 @@ const favorites = [
   }
 ];
 
-// const ratings = [
-//   {
-//     rating: 3,
-//     favoriteId: 1,
-//     profileId: 1 
-//   }
-// ];
-
 client.query(`
   INSERT INTO profile (username, hash)
   VALUES ($1, $2)
@@ -41,22 +33,6 @@ client.query(`
       })
     );
   })
-  // .then(result => {
-  //   console.log(result);
-  //   const profile = result[0][0];
-  //   const favorite = result[0][0];
-  //   return Promise.all(
-  //     ratings.map(rating => {
-  //       return client.query(`
-  //     INSERT INTO ratings (rating, favorite_id, profile_id)
-  //     VALUES ($1, $2, $3)
-  //     RETURNING id, rating, favorite_id as "favoriteId", profile_id as "profileId";
-  //   `,
-  //       [rating.rating, favorite.id, profile.id])
-  //         .then(result => result.rows[0].id);
-  //     })
-  //   );
-  // })
 
   .then(
     () => console.log('seed data load complete'),
